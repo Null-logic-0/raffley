cbc79a156bb60b9614f6a5bf0789be13075c8f42
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2026-03-17 17:29:51 +0400

    Add admin raffle LiveViews and Admin context
    Introduce an Admin context with CRUD functions for raffles and new LiveView-based admin UI. Adds Raffley.Admin (list/create/get/update/delete/change), AdminRaffleLive.Index and AdminRaffleLive.Form for listing, creating, editing and deleting raffles (uses streams, JS push for delete and hide). Registers admin routes in the router and adds an Admin link to the top-level layout. Also tightens Raffle validations (description min length and ticket_price >= 1) and updates .iex.exs imports; convert a few links to use ~p path sigils.

---
+++ lib/raffley/admin.ex
@@ -0,0 +1,31 @@
+defmodule Raffley.Admin do
+  import Ecto.Query
+  alias Raffley.Raffles.Raffle
+  alias Raffley.Repo
+
+  def list_raffles do
+    Raffle
+    |> order_by(desc: :inserted_at)
+    |> Repo.all()
+  end
+
+  def create_raffle(attrs \\ %{}) do
+    %Raffle{}
+    |> Raffle.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def change_raffle(%Raffle{} = raffle, attrs \\ %{}) do
+    Raffle.changeset(raffle, attrs)
+  end
+
+  def get_raffle!(id), do: Repo.get!(Raffle, id)
+
+  def update_raffle(%Raffle{} = raffle, attrs) do
+    raffle
+    |> Raffle.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_raffle(%Raffle{} = raffle), do: Repo.delete(raffle)
+end
---
+++ lib/raffley_web/live/admin_raffle_live/index.ex
@@ -0,0 +1,82 @@
+defmodule RaffleyWeb.AdminRaffleLive.Index do
+  use RaffleyWeb, :live_view
+
+  import RaffleyWeb.CustomComponents
+  alias Raffley.Admin
+
+  def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign(:page_title, "Listing Raffles")
+      |> stream(:raffles, Admin.list_raffles())
+
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div class="admin-index">
+      <.button phx-click={
+        JS.toggle(
+          to: "#joke",
+          in: {"ease-in-out duration-300", "opacity-0", "opacity-100"},
+          out: {"ease-in-out duration-300", "opacity-100", "opacity-0"},
+          time: 300
+        )
+      }>
+        Toggle Joke
+      </.button>
+      <div id="joke" class="joke hidden">
+        What's a tree's favorite drink?
+      </div>
+      <.header class="mt-6">
+        {@page_title}
+        <:actions>
+          <.link navigate={~p"/admin/raffles/new"} class="button">
+            New Raffle
+          </.link>
+        </:actions>
+      </.header>
+      <.table
+        id="raffles"
+        rows={@streams.raffles}
+        row_click={fn {_, raffle} -> JS.navigate(~p"/raffles/#{raffle}") end}
+      >
+        <:col :let={{_dom_id, raffle}} label="Prize">
+          <.link navigate={~p"/raffles/#{raffle}"}>
+            {raffle.prize}
+          </.link>
+        </:col>
+        <:col :let={{_dom_id, raffle}} label="Status">
+          <.badge status={raffle.status} />
+        </:col>
+        <:col :let={{_dom_id, raffle}} label="Ticket Price">
+          {raffle.ticket_price}
+        </:col>
+
+        <:action :let={{_dom_id, raffle}}>
+          <.link navigate={~p"/admin/raffles/#{raffle}/edit"}>
+            Edit
+          </.link>
+        </:action>
+        <:action :let={{dom_id, raffle}}>
+          <.link phx-click={delete_and_hide(dom_id, raffle)} data-confirm="Are you sure?">
+            Delete
+          </.link>
+        </:action>
+      </.table>
+    </div>
+    """
+  end
+
+  def handle_event("delete", %{"id" => id}, socket) do
+    raffle = Admin.get_raffle!(id)
+    {:ok, _} = Admin.delete_raffle(raffle)
+    {:noreply, stream_delete(socket, :raffles, raffle)}
+  end
+
+  def delete_and_hide(dom_id, raffle) do
+    JS.push("delete", value: %{id: raffle.id})
+    |> JS.hide(to: "##{dom_id}", transition: "fade-out")
+  end
+end
